878c835b27c34dd567b1fc85d374191726e8de3c
Tina Gao <tina.hm.gao@gmail.com>
2019-11-28 11:18:07 +1300

    Add to-top button, remove unused links

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -5,6 +5,7 @@ class StickyHeader {
   constructor() {
     this.siteHeader = document.querySelector('.site-header')
     this.pageSections = document.querySelectorAll('.page-section')
+    this.toTop = document.querySelector('.to-top')
     this.browserHeight = window.innerHeight
     this.previousScrollY = window.scrollY
     this.events()
@@ -28,8 +29,10 @@ class StickyHeader {
 
     if (window.scrollY > 60) {
       this.siteHeader.classList.add('site-header--dark')
+      this.toTop.classList.add('to-top--is-visible')
     } else {
       this.siteHeader.classList.remove('site-header--dark')
+      this.toTop.classList.remove('to-top--is-visible')
     }
 
     this.pageSections.forEach(el => this.calcSection(el))
